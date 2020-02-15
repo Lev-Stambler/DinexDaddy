796b51c174696650a43251352314ef169e573d20
shivaPeri <57304890+shivaPeri@users.noreply.github.com>
2020-02-14 21:39:11 -0500

    list view

---
+++ lib/Classes/Seller.dart
@@ -0,0 +1,12 @@
+class Seller {
+  String name;
+  String email;
+  int dinexPrice;
+  int blockPrice;
+  Seller(String name, String email, int dinex, int block){
+    name = name;
+    email = email;
+    dinexPrice = dinex;
+    blockPrice = block;
+  }
+}

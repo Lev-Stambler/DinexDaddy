7e0a99d3cd1d112784c2a95439e043c6ece99538
Saral Tayal <thunderbird211@gmail.com>
2020-02-15 11:24:23 -0500

    sdf

--- lib/Screens/BuyerMatch.dart
+++ lib/Screens/BuyerMatch.dart
@@ -2,9 +2,35 @@ import 'package:flutter/material.dart';
 import '../Classes/Seller.dart';
 
 class BuyerMatch extends StatelessWidget {
-  List<Seller> Sellers = [];
+  List<Seller> Sellers = [
+    Seller("pp", "pp@smol.com", 10, 15),
+    
+    Seller("shSmol", "pp@smol.com", 1, 5),
+  ];
+  List<ListView> ListToDisplay = [];
+  buildList(){
+    for(var i = 0; i < Sellers.length; i++){
+      ListToDisplay.add(
+        ListView(
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Text("FUCK THIS SHIT")
+                // Text(Sellers[i].name),
+                // Text(Sellers[i].email),
+                // Text(Sellers[i].dinexPrice.toString()),      
+                // Text(Sellers[i].blockPrice.toString()),      
+              ],
+              ),
+          ],
+        )
+      );
+
+    }
+  }
  @override
   Widget build(BuildContext context) {
+    buildList();
     return Scaffold(
       appBar: AppBar(
         title: Text("Matches"),
@@ -18,15 +44,8 @@ class BuyerMatch extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: <Widget>[
-                for (var seller = Sellers[0])
-                {
-
-                }
                 ListView(
-                  children: <Widget>[
-                    Text("Element 1"),
-                    Text("Element 2")
-                  ],
+                  children: ListToDisplay
                 ),
               ],
             )
